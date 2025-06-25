2a51e08afcf1cb483a5a746212dd4c436e9f1a02
James B. <james.bartnik@gmail.com>
2025-06-24 17:14:15 -0700

    Adds metatags

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -13,10 +13,15 @@ export const Route = createRootRoute({
         <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-4">
           <Link
             to="/"
-            className="text-2xl font-bold tracking-tight text-blue-600 hover:opacity-80"
+            className="text-2xl font-bold tracking-tight text-blue-600 hover:opacity-90"
           >
-            James Bartnik
+            <img
+              src={"/favicon.svg"}
+              alt={"JB Logo"}
+              className="h-full w-16 object-cover"
+            />
           </Link>
+
           <nav className="flex gap-6">
             {navLinks.map(({ name, to }) => (
               <Link
